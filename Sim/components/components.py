53fb70722252bcbffe2f6a67e2052a0d08971a78
Chris Whitworth <chris.whitworth@gmail.com>
2013-11-21 22:01:01 +0000

    Remove adder/half adder from components

--- Sim/components/components.py
+++ Sim/components/components.py
@@ -19,17 +19,3 @@ class Clock:
             self.out.setState(LOW)
             self.simulation.addActionAfter(self.clock, self.tLow)
 
-class HalfAdder:
-    def __init__(self, a, b, s, c, simulation):
-        self.sumCalc = XOR(a, b, s, simulation)
-        self.carryCalc = AND(a, b, c, simulation)
-
-class FullAdder:
-    def __init__(self, a, b, cIn, s, cOut, simulation, monitor=None):
-        ha1Sum = Wire("", monitor)
-        ha1Carry = Wire("", monitor)
-        ha2Carry = Wire("", monitor)
-
-        self.ha1 = HalfAdder(a, b, ha1Sum, ha1Carry, simulation)
-        self.ha2 = HalfAdder(cIn, ha1Sum, s, ha2Carry, simulation)
-        self.carry = OR(ha1Carry, ha2Carry, cOut, simulation)
