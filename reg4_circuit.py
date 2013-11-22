fbe874fb1db3a98228ca169b4f0992aa97465d55
Chris Whitworth <chris.whitworth@red-gate.com>
2013-11-22 16:30:14 +0000

    Teeny comments

--- reg4_circuit.py
+++ reg4_circuit.py
@@ -17,16 +17,18 @@ reg = registers.R4_FET(IN, OUT, CLK, s)
 for i in range(4):
     IN[i].setState(LOW)
 
+# Bus is undefined until t = 100 (clock falling edge)
+
 s.runUntil(125)
 
-print( OUT.read())
+print( OUT.read()) # 0
 
 IN[3].setState(HIGH)
 IN[1].setState(HIGH)
 s.runUntil(175)
 
-print( OUT.read())
+print( OUT.read()) # 0
 
 s.runUntil(225)
 
-print( OUT.read())
+print( OUT.read()) # 2_1010
