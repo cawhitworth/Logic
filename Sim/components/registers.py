707fef1c9c843e0764c69e9fc16e4bf850dcbbe2
Chris Whitworth <chris.whitworth@red-gate.com>
2013-12-10 11:57:14 +0000

    Arbitrary width registers

--- Sim/components/registers.py
+++ Sim/components/registers.py
@@ -13,3 +13,14 @@ class R4_FET:
         ffs = [ flipflops.D_MS_FET(B_IN[i], CLK, B_OUT[i], B_OUTi[i], sim)
                 for i in range(4) ]
 
+class R_FET:
+    def __init__(self, B_IN, B_OUT, CLK, sim):
+        if len(B_IN) != len(B_OUT):
+            raise ValueError("Input and output buses must be equal width")
+
+        width = len(B_IN)
+
+        B_OUTi = Bus(width)
+
+        ffs = [ flipflops.D_MS_FET(B_IN[i], CLK, B_OUT[i], B_OUTi[i], sim)
+                for i in range(width) ]
