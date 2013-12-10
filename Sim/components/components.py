d077fd13d1cdcb8b08c782438a6cbc1a44bb2d51
Chris Whitworth <chris.whitworth@red-gate.com>
2013-12-10 16:31:42 +0000

    A bus buffer (for connecting buses together, with a delay; in the real world, this would also be an amplifier...)

--- Sim/components/components.py
+++ Sim/components/components.py
@@ -2,32 +2,47 @@ from Sim.gates import AND, OR, NOT, XOR
 from Sim.sim import Wire, HIGH, LOW
 
 class Clock:
-    def __init__(self, out, tHigh, tLow, simulation):
+    def __init__(self, out, tHigh, tLow, sim):
         self.out = out
         self.tHigh = tHigh
         self.tLow = tLow
-        self.simulation = simulation
+        self.sim = sim
 
-        simulation.addActionAfter( lambda : out.setState(LOW), 0)
-        simulation.addActionAfter(self.clock, tLow)
+        sim.addActionAfter( lambda : out.setState(LOW), 0)
+        sim.addActionAfter(self.clock, tLow)
 
     def clock(self):
         if self.out.state == LOW:
             self.out.setState(HIGH)
-            self.simulation.addActionAfter(self.clock, self.tHigh)
+            self.sim.addActionAfter(self.clock, self.tHigh)
         else:
             self.out.setState(LOW)
-            self.simulation.addActionAfter(self.clock, self.tLow)
+            self.sim.addActionAfter(self.clock, self.tLow)
 
 class Buffer:
-    def __init__(self, I, O, simulation):
+    def __init__(self, I, O, sim):
         I.connect(self)
         self.O = O
+        self.sim = sim
 
     def notify(self, wire):
         futureState = wire.state
         action = lambda : self.outputWire.setState(futureState)
-        self.simulation.addActionAfter(action, 1, self.O)
+        self.sim.addActionAfter(action, 1, self.O)
 
+class BusBuffer:
+    def __init__(self, I, O, sim):
+        if len(I) != len(O):
+            raise ValueError("Input and output buses must be same width")
+        self.O = {}
+        self.sim = sim
+        for wire in range(len(I)):
+            I[wire].connect(self)
+            self.O[ I[wire] ] = O[wire]
+
+    def notify(self, wire):
+        futureState = wire.state
+        action = lambda : self.O[wire].setState(futureState)
+        self.sim.addActionAfter(action, 1, self.O[wire])
 
 
